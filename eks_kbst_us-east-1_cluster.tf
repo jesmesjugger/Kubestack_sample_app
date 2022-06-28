1b23722d6c7a87a8350346542f744a4695ef3723
Martin Mkolwe <martinjj80@gmail.com>
2022-06-28 12:52:53 +0300

    Modifying the cluster ec2 instance types and max value

--- eks_kbst_us-east-1_cluster.tf
+++ eks_kbst_us-east-1_cluster.tf
@@ -11,8 +11,8 @@ module "eks_kbst_us-east-1" {
       base_domain = var.base_domain
       cluster_availability_zones = "us-east-1a,us-east-1b,us-east-1c,us-east-1d,us-east-1f"
       cluster_desired_capacity = 3
-      cluster_instance_type = "m5a.xlarge"
-      cluster_max_size = 9
+      cluster_instance_type = "t3.medium"
+      cluster_max_size = 6
       cluster_min_size = 3
       name_prefix = "kbst"
     }
